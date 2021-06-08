606ed465c5887130590e1c7363c8d4204f3d29ee
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-08 14:10:50 -0700

    fix bug to return 0 when second input empty

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,32 @@
-// Business Logic
+// // Business Logic
 
+// function wordCounter(text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   let wordCount = 0;
+//   const wordArray = text.split(" ");
+//   wordArray.forEach(function(element) {
+//     if (!Number(element)) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
+
+// function numberOfOccurrencesInText(word, text) {
+//   if (text.trim().length === 0) {
+//     return 0;
+//   }
+//   const wordArray = text.split(' ');
+//   let wordCount = 0;
+//   wordArray.forEach(function(element) {
+//     if (word.toLowerCase() === element.toLowerCase()) {
+//       wordCount++;
+//     }
+//   });
+//   return wordCount;
+// }
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -15,15 +42,29 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if (text.trim().length === 0) {
+  if ((text.trim().length === 0) || (word.trim().length === 0)) {
     return 0;
   }
-  const wordArray = text.split(' ');
+  const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
-    if (word.toLowerCase() === element.toLowerCase()) {
+    if (element.toLowerCase() .includes(word.toLowerCase())) {
       wordCount++;
+    
     }
   });
   return wordCount;
 }
+// UI Logic
+
+$(document).ready(function(){
+  $("form#word-counter").submit(function(event){
+    event.preventDefault();
+    const passage = $("#text-passage").val();
+    const word = $("#word").val();
+    const wordCount = wordCounter(passage);
+    const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
+    $("#total-count").html(wordCount);
+    $("#selected-count").html(occurrencesOfWord);
+  });
+});
