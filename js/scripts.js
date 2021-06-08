da4cc4b145641c0e50c497e87b7d879a5f391087
Jennifer Bordon <jennifer.bordon@gmail.com>
2021-06-08 14:50:59 -0700

    add finalized example files

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,11 @@
-// // Business Logic
+// Utility Logic
 
-// function wordCounter(text) {
-//   if (text.trim().length === 0) {
-//     return 0;
-//   }
-//   let wordCount = 0;
-//   const wordArray = text.split(" ");
-//   wordArray.forEach(function(element) {
-//     if (!Number(element)) {
-//       wordCount++;
-//     }
-//   });
-//   return wordCount;
-// }
+function noInputtedWord(word, text) {
+  return ((text.trim().length === 0) || (word.trim().length === 0));
+}
 
-// function numberOfOccurrencesInText(word, text) {
-//   if (text.trim().length === 0) {
-//     return 0;
-//   }
-//   const wordArray = text.split(' ');
-//   let wordCount = 0;
-//   wordArray.forEach(function(element) {
-//     if (word.toLowerCase() === element.toLowerCase()) {
-//       wordCount++;
-//     }
-//   });
-//   return wordCount;
-// }
+// Business Logic
+
 function wordCounter(text) {
   if (text.trim().length === 0) {
     return 0;
@@ -42,20 +21,39 @@ function wordCounter(text) {
 }
 
 function numberOfOccurrencesInText(word, text) {
-  if ((text.trim().length === 0) || (word.trim().length === 0)) {
+  if (noInputtedWord(word, text)) {
     return 0;
   }
   const wordArray = text.split(" ");
   let wordCount = 0;
   wordArray.forEach(function(element) {
-    if (element.toLowerCase() .includes(word.toLowerCase())) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
       wordCount++;
-    
     }
   });
   return wordCount;
 }
+
 // UI Logic
+
+function boldPassage(word, text) {
+  if (noInputtedWord(word, text)) {
+    return "";
+  }
+  let htmlString = "<p>";
+  let textArray = text.split(" ");
+  textArray.forEach(function(element, index) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      htmlString = htmlString.concat("<b>" + element + "</b>");
+    } else {
+      htmlString = htmlString.concat(element);
+    }
+    if (index !== (textArray.length - 1)) {
+      htmlString = htmlString.concat(" ");
+    }
+  });
+  return htmlString + "</p>";
+}
 
 $(document).ready(function(){
   $("form#word-counter").submit(function(event){
@@ -66,5 +64,6 @@ $(document).ready(function(){
     const occurrencesOfWord = numberOfOccurrencesInText(word, passage);
     $("#total-count").html(wordCount);
     $("#selected-count").html(occurrencesOfWord);
+    $("#bolded-passage").html(boldPassage(word, passage));
   });
 });
